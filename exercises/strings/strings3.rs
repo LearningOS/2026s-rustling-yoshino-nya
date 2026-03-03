357435b727e80488de78aed9bcb7031f660802b0
yoshino-nya <wu_yi_@outlook.com>
2026-03-03 08:48:46 +0800

    finish: enums, hashmaps, strings and modules

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,38 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut start = 0;
+    for c in input.chars() {
+        if c == ' ' {
+            start += c.len_utf8();
+        } else {
+            break;
+        }
+    }
+    let mut end = input.len();
+    for c in input.chars().rev() {
+        if c == ' ' {
+            end -= c.len_utf8()
+        } else {
+            break;
+        }
+    }
+    String::from(&input[start..end])
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut res = String::from(input);
+    res.push_str(" world!");
+    res
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -39,7 +56,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
