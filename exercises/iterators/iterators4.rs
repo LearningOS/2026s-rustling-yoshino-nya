33ce7e16a40dac336fdc9104b96fa57baf79bb8e
yoshino-nya <wu_yi_@outlook.com>
2026-03-04 17:57:30 +0800

    finish: iterators and lifetimes

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,13 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // let mut res = 1;
+    // // for i in (1..num + 1) {
+    // //     res *= i;
+    // // }
+    // (1..num + 1).into_iter().map(|i| res *= i);
+    // res
+    (1..=num).fold(1, |acc, x| acc * x)
 }
 
 #[cfg(test)]
